621507cd39acae9e501d920435ba05979f1a00f7
barbarasagredo <barpaz.sc@gmail.com>
2021-03-04 09:36:19 -0300

    Final Ceasar Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
 const cipher = {
-  // ...
-};
+  //CIFRADO PARA PADRES
+  encode: (offset, codigo) => {
 
+    let upperCase = codigo.toUpperCase(); //pasa a mayúscula los carácteres
+    let finalMessage = ''; //declara que el mensaje debe ser escrito
+    let desplazamiento = parseInt(offset); //devuelve el valor entero al offset ingresado
+
+    for (let i = 0; i < upperCase.length; i++) { //desde 0 recorriendo la cadena de caracteres
+      let letterInAscii = upperCase.charCodeAt(i); //indica el valor ascii de cada caracter
+      let newLetterInAscii = 0;
+      if (letterInAscii == 32) {
+        newLetterInAscii = 32; //deja el valor ascii 32 como espacio
+      } else {
+        newLetterInAscii = ((letterInAscii - 65 + desplazamiento) % 26) + 65; // indica el valor ascii con el desplazamiento incluído
+      }
+      let letterEncoded = String.fromCharCode(newLetterInAscii); // devuelve el valor de cada letra en ascii
+
+      finalMessage = finalMessage + letterEncoded;
+    }
+    return finalMessage;
+  },
+
+  //DESCIFRADO PARA NIÑOS
+  decode: (offset, codigo) => {
+    let upperCase = codigo.toUpperCase(); //pasa a mayúscula los carácteres
+    let finalMessage = '';
+    let desplazamiento = parseInt(offset);
+
+    for (let i = 0; i < upperCase.length; i++) {
+      let letterInAscii = upperCase.charCodeAt(i); //indica el valor ascii de cada caracter
+      let newLetterInAscii = 0;
+      if (letterInAscii == 32) {
+        newLetterInAscii = 32;
+      } else {
+        newLetterInAscii = ((letterInAscii + 65 - desplazamiento) % 26) + 65; // indica el valor ascii con el desplazamiento incluído
+      }
+      let letterEncoded = String.fromCharCode(newLetterInAscii);
+
+      finalMessage = finalMessage + letterEncoded;
+    }
+
+    return finalMessage;
+  }
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const codificar = () => {
+  let codigo = document.getElementById("letras").value;
+  let offset = document.getElementById("numeros").value;
+  let mensaje = cipher.encode(offset, codigo);
+  document.getElementById("cajaTexto").innerHTML = mensaje;
+}
+document.getElementById("botonDescubre").onclick = function () { codificar(); }
+
+const decodificar = () => {
+  let codigo = document.getElementById("letras").value;
+  let offset = document.getElementById("numeros").value;
+  let mensaje = cipher.decode(offset, codigo);
+  document.getElementById("cajaTexto").innerHTML = mensaje;
+}
+document.getElementById("cifrando").onclick = function () { decodificar(); }
+
+
+//funcion de navegacion pagina 1 a pagina 2
+let botonInicio = document.querySelector("#botonInicio"); //boton inicio
+botonInicio.addEventListener("click", nextPage);
+
+function nextPage() {
+  document.querySelector(".card").style.display = "none"; // none bloquea la página
+  document.querySelector(".card2").style.display = "block"; // muestra la página 
+  document.querySelector(".card3").style.display = "none";
+  document.querySelector(".card4").style.display = "none";
+}
+
+//funcion ir a mensaje para padres
+let goToParentMessage = document.querySelector("#parentalMessage");
+goToParentMessage.addEventListener("click", changeToPArentMessage);
+
+function changeToPArentMessage() {
+  document.querySelector(".card").style.display = "none";
+  document.querySelector(".card2").style.display = "none";
+  document.querySelector(".card3").style.display = "block";
+  document.querySelector(".card4").style.display = "none";
+}
+
+//boton compartir a redes sociales
+let shareRRSS = document.querySelector("#share");
+shareRRSS.addEventListener("click", shareTheResult);
+
+function shareTheResult() {
+  document.querySelector(".card").style.display = "none";
+  document.querySelector(".card2").style.display = "none";
+  document.querySelector(".card3").style.display = "none";
+  document.querySelector(".card4").style.display = "block";
+}
+
+//volver al inicio
+let backTofirst = document.querySelector("#volver");
+backTofirst.addEventListener("click", allOverAgain);
+
+function allOverAgain() {
+  document.querySelector(".card").style.display = "block";
+  document.querySelector(".card2").style.display = "none";
+  document.querySelector(".card3").style.display = "none";
+  document.querySelector(".card4").style.display = "none";
+}
+
+//cambiar segundo texto para padres
+function cambiarTexto() {
+  document.getElementById("primer").innerHTML = 'La finalidad del código secreto es hacer un acercamiento simple para que les niñes descubran el mundo tecnológico por medio de un cifrado de texto simple. <br> Para ello, escribe tu mensaje de amor, el que te dará un código, el cual puedes entregarle a tu hije. Al ingresar el código, descubrirá el mensaje real. <br> Escribe tu mensaje <a href = "#card2"> AQUÍ';
+}
+document.getElementById("boton").addEventListener("click", cambiarTexto);
+
